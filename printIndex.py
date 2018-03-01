6ef9e75401c775b76563f537807defed03108514
Ganesh S S <ganeshsuriya24@gmail.com>
2018-03-01 18:13:10 +0530

    Update printIndex.py

--- printIndex.py
+++ printIndex.py
@@ -2,6 +2,6 @@ numToFind=input().strip()
 array,visited=input().split(" "),-1
 for i in range(0,len(array)):
     if array[i]==numToFind:
-        visitied=i
+        visited=i
         break
 print(visited)
